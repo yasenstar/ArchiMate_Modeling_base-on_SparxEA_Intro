a06ee338efb46001a312c431782797277cf6706d
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2024-09-11 11:25:50 +0800

    add mindmap

---
+++ ArchiMate Modeling base on Sparx EA Introduction.md
@@ -0,0 +1,59 @@
+ ArchiMate Modeling base on Sparx EA Introduction
+
+# Basic Viewpoints
+
+## 01 Organization
+
+## 02 Business Process Cooperation
+
+## 03 Product
+
+## 04 Application Coorperation
+
+## 05 Application Usage
+
+## 06 Impelmentation and Deployment
+
+## 07 Technology
+
+## 08 Technology Usage
+
+## 09 Information Structure
+
+## 10 Service Realization
+
+## 11 Physical
+
+## 12 Layered
+
+# Motivation Viewpoint
+
+## 13 Stakeholder
+
+## 14 Goal Realization
+
+## 15 Goal Contribution
+
+## 16 Principles Viewpoint
+
+## 17 Requirements Realization
+
+## 18 Motivation
+
+# Strategy Viewpoint
+
+## 19 Strategy
+
+## 20 Capability Map
+
+## 21 Resource Map
+
+## 22 Outcome Realization
+
+# Implementation and Migration Viewpoints
+
+## 23 Project
+
+## 24 Migration
+
+## 25 Implementation and Migration
---
+++ ArchiMate Modeling base on Sparx EA Introduction.mm
@@ -0,0 +1,113 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="ArchiMate Modeling base on Sparx EA Introduction" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1726024151411"><hook NAME="MapStyle" background="#f5f5dc">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_sepia_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1558627382" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#2c2b29" BACKGROUND_COLOR="#eedfcc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1558627382" STARTINCLINATION="81.6 pt;-10.8 pt;" ENDINCLINATION="81.6 pt;19.8 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="9" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10" ITALIC="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#2c2b29" BACKGROUND_COLOR="#ffffff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_411331447" COLOR="#ffffff" BACKGROUND_COLOR="#bf616a" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_411331447"/>
+<font NAME="Ubuntu" SIZE="12" BOLD="true"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#2c2b29" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" BACKGROUND_COLOR="#eedfcc" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="Basic Viewpoints" POSITION="bottom_or_right" ID="ID_671918920" CREATED="1726024695118" MODIFIED="1726024702783">
+<node TEXT="01 Organization" POSITION="bottom_or_right" ID="ID_349387977" CREATED="1726024722364" MODIFIED="1726024743785"/>
+<node TEXT="02 Business Process Cooperation" POSITION="bottom_or_right" ID="ID_1363044328" CREATED="1726024743950" MODIFIED="1726024755456"/>
+<node TEXT="03 Product" POSITION="bottom_or_right" ID="ID_1605347505" CREATED="1726024755614" MODIFIED="1726024758359"/>
+<node TEXT="04 Application Coorperation" POSITION="bottom_or_right" ID="ID_301377610" CREATED="1726024758513" MODIFIED="1726024767735"/>
+<node TEXT="05 Application Usage" POSITION="bottom_or_right" ID="ID_1898042333" CREATED="1726024767911" MODIFIED="1726024772998"/>
+<node TEXT="06 Impelmentation and Deployment" POSITION="bottom_or_right" ID="ID_1882795384" CREATED="1726024773118" MODIFIED="1726024778929"/>
+<node TEXT="07 Technology" POSITION="bottom_or_right" ID="ID_627027407" CREATED="1726024779100" MODIFIED="1726024782544"/>
+<node TEXT="08 Technology Usage" POSITION="bottom_or_right" ID="ID_324699713" CREATED="1726024782664" MODIFIED="1726024786982"/>
+<node TEXT="09 Information Structure" POSITION="bottom_or_right" ID="ID_1923360354" CREATED="1726024787114" MODIFIED="1726024791852"/>
+<node TEXT="10 Service Realization" POSITION="bottom_or_right" ID="ID_1290045225" CREATED="1726024792008" MODIFIED="1726024800386"/>
+<node TEXT="11 Physical" POSITION="bottom_or_right" ID="ID_1739416995" CREATED="1726024800547" MODIFIED="1726024803343"/>
+<node TEXT="12 Layered" POSITION="bottom_or_right" ID="ID_1456861138" CREATED="1726024803485" MODIFIED="1726024806415"/>
+</node>
+<node TEXT="Motivation Viewpoint" POSITION="bottom_or_right" ID="ID_169212183" CREATED="1726024703567" MODIFIED="1726024708917">
+<node TEXT="13 Stakeholder" ID="ID_1102134672" CREATED="1726024808252" MODIFIED="1726024812880"/>
+<node TEXT="14 Goal Realization" ID="ID_1004721397" CREATED="1726024813015" MODIFIED="1726024817335"/>
+<node TEXT="15 Goal Contribution" ID="ID_1982888972" CREATED="1726024828543" MODIFIED="1726024833463"/>
+<node TEXT="16 Principles Viewpoint" ID="ID_540585105" CREATED="1726024833623" MODIFIED="1726024843631"/>
+<node TEXT="17 Requirements Realization" ID="ID_204484108" CREATED="1726024843782" MODIFIED="1726024849113"/>
+<node TEXT="18 Motivation" ID="ID_887924714" CREATED="1726024849266" MODIFIED="1726024852334"/>
+</node>
+<node TEXT="Strategy Viewpoint" POSITION="bottom_or_right" ID="ID_1640400395" CREATED="1726024709102" MODIFIED="1726024713360">
+<node TEXT="19 Strategy" ID="ID_425639414" CREATED="1726024857479" MODIFIED="1726024867750"/>
+<node TEXT="20 Capability Map" ID="ID_1010388978" CREATED="1726024860150" MODIFIED="1726024872375"/>
+<node TEXT="21 Resource Map" ID="ID_455959056" CREATED="1726024884870" MODIFIED="1726024888643"/>
+<node TEXT="22 Outcome Realization" ID="ID_444724166" CREATED="1726024888776" MODIFIED="1726024894264"/>
+</node>
+<node TEXT="Implementation and Migration Viewpoints" POSITION="bottom_or_right" ID="ID_649432027" CREATED="1726024713527" MODIFIED="1726024721930">
+<node TEXT="23 Project" ID="ID_1195733018" CREATED="1726024901672" MODIFIED="1726024913721"/>
+<node TEXT="24 Migration" ID="ID_1630208646" CREATED="1726024914463" MODIFIED="1726024917319"/>
+<node TEXT="25 Implementation and Migration" ID="ID_1338091735" CREATED="1726024917487" MODIFIED="1726024927688"/>
+</node>
+</node>
+</map>
